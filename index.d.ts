28d43ddb4e83299f48bb85dd1ba76db1b1bf04c7
pellejacobs <pellejacobs@hotmail.com>
2016-08-26 10:07:40 +0200

    Add comment concerning weird import of constants in index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,3 +1,4 @@
+// importing constants, to make sure they are included in the bundle process
 import * as constants from './constants'
 
 import { Store, StoreEnhancer, Reducer } from 'redux'
@@ -11,7 +12,7 @@ interface PersistStoreConfig<S> {
   blacklist?: string[]
   whitelist?: string[]
   /**
-   * a storage object is an object that implements setItem, getItem, removeItem and getAllKeys 
+   * a storage object is an object that implements setItem, getItem, removeItem and getAllKeys
    */
   storage?: any
   transforms?: Transform<S>[]
@@ -29,8 +30,8 @@ export interface Transform<S> {
 export interface Persistor {
   /**
    * Remove the stored state of certain keys.
-   * 
-   * @param keys Keys to purge 
+   *
+   * @param keys Keys to purge
    */
   purge: (keys: string[]) => void
   /**
@@ -41,10 +42,10 @@ export interface Persistor {
   resume: () => void
   /**
    * Rehydrate some state into the store
-   * 
+   *
    * @param incoming State to rehyrdrate into the store
    * @param [options] Has a serial option to define whether the incoming is serialized
-   * 
+   *
    * @returns the state that got hydrated into the the store
    */
   rehydrate: (incoming: {} | string, options?: { serial: boolean }) => {}
@@ -59,20 +60,20 @@ export function createTransform<S>(inbound: Reducer<S>, outbound: Reducer<S>, co
 
 /**
  * @param [config] Has a log option enable logging to the console
- * 
- * @returns a StoreEnhancer that will automatically shallow merge the persisted state for each key. It additionally 
- *  queues any actions that are dispatched before rehydration is complete, and fires them after rehydration is 
- *  finished. 
+ *
+ * @returns a StoreEnhancer that will automatically shallow merge the persisted state for each key. It additionally
+ *  queues any actions that are dispatched before rehydration is complete, and fires them after rehydration is
+ *  finished.
  */
 export function autoRehydrate<S>(config?: { log: boolean }): StoreEnhancer<S>
 
 /**
  * Enable automatic persisting of a redux store
- * 
+ *
  * @param store Redux store to be persisted
  * @param [config] Object with configuration
  * @param [callback] Function called on completion
- * 
+ *
  * @returns Persistor object to manipulate the state further
  */
 export function persistStore<S>(store: Store<S>, config?: PersistStoreConfig<S>, callback?: () => any): Persistor
